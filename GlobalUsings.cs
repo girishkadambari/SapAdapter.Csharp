1c585483ec5aabd907548105a4811a8a53de5ecb
girish <girish99126@gmail.com>
2026-03-01 16:15:44 +0530

    fix

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -5,3 +5,9 @@ global using System.IO;
 global using System.Linq;
 global using System.Threading;
 global using System.Threading.Tasks;
+
+// Polyfill: Required for C# 9+ record/init support on .NET Framework 4.8
+namespace System.Runtime.CompilerServices
+{
+    internal static class IsExternalInit { }
+}
